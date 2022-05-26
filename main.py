7f55c3ae639dd3f488d839f2ba8b2d0e49440e84
Aleksandr90210 <98108166+Aleksandr90210@users.noreply.github.com>
2022-05-26 17:53:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 def input_f():
     while True:
-        n = int(input('Введите натуральное число'))
+        x = int(input('Введите натуральное число'))
         if n > 0:
             break
         print('Неверные даные')
-    return n
+    return x
 
-def x2(n):
-    return collatz(n // 2)
+def x2(x):
+    return collatz(x // 2)
 
-def x3_1n(n):
-    return collatz(n*3+1)
-def collatz(n):
-    result = [n]
+def x3_1n(x):
+    return collatz(x*3+1)
+
+def collatz(x):
+    result = [x]
     if n == 1:
         pass
-    elif n % 2 == 0:
+    elif x % 2 == 0:
         result.extend(x2(n))
     else:
         result.extend(x3_1(n))
     return result
+
+if __name__ == '__main__':
+    a = inputs()
+    print(collatz(a))
